fa83330ad211f9cf9a0bc9fd0638302cba147b05
Travis Crocker <Travis.Crocker@outlook.com>
2021-11-24 17:13:09 -0500

    added readme

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -37,12 +37,6 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const productData = await Product.create(req.body);
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
